6ea7599309a8d6f5b43edd9b86bb2f73d4d3f9de
Ting-Wei Su <twsu@cm-matlab.stanford.edu>
2018-03-01 15:45:18 -0900

    ??

--- code/arch_search.py
+++ code/arch_search.py
@@ -19,8 +19,8 @@ if args.setting == 'convnet':
     """
     arch_search_convnet(
         start_net_path='../start_nets/start_net_convnet_small_C10+',
-        arch_search_folder='../arch_search/Convnet/C10+/Conv_C10+_rl_small',
-        net_pool_folder='../net_pool/Convnet/C10+/Conv_C10+_rl_small',
+        arch_search_folder='../arch_search/Convnet/C10+/Conv_C10+_baseline',
+        net_pool_folder='../net_pool/Convnet/C10+/Conv_C10+_baseline',
         max_episodes=15,
         random=False,
     )
