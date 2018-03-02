959e8b1c034e502fb217d0769d2b6e67eaf88e34
Ting_wei <crazyst39111@gmail.com>
2018-03-01 17:13:14 -0800

    test

--- code/models/utils.py
+++ code/models/utils.py
@@ -72,13 +72,13 @@ class RunConfig:
         if dataset in ['C10', 'C10+', 'C100', 'C100+']:
             run_config = {
                 'batch_size': 64,
-                'n_epochs': 3,
+                'n_epochs': 300,
                 'init_lr': 0.1,
                 'reduce_lr_epochs': [0.5, 0.75],  # epochs * 0.5, epochs * 0.75
                 'reduce_lr_factors': [10, 10],
                 'opt_config': ['momentum', {'momentum': 0.9, 'use_nesterov': True}],
                 'dataset': dataset,  # choices = [C10, C10+, C100, C100+]
-                'validation_size': None,  # None or int
+                'validation_size': 10000,  # None or int
                 'validation_frequency': 10,
                 'shuffle': 'every_epoch',  # None, once_prior_train, every_epoch
                 'normalization': 'by_channels',  # None, divide_256, divide_255, by_channels
